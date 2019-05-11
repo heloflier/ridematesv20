79130eaa747edc8e720276928fe619e932ef42bb
Fabio <heloflyer@hotmail.com>
2019-05-10 17:38:24 -0700

    created profile component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Profile from './components/Profile';
 
 function App() {
     return (
         <div className="App">
             Ridemates v 2.0
-    </div>
+            <Profile />
+        </div>
     );
 }
 
