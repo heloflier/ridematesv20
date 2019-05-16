aca2f70034541fde3751b098c6fb30ecbb3c87c7
Fabio <heloflyer@hotmail.com>
2019-05-16 15:46:31 -0700

    completed basic form functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,15 +3,42 @@ import React, { Component } from 'react';
 class LoginForm extends Component {
     state = {
         email: '',
-        password: ''
+        pwd: '',
+        emailError: false,
+        pwdError: false
     }
 
-    onChange = (e) => {
+    onChange = e => {
         console.log('ONCHAMGE', e.target.value)
-        const email = e.target.value;
-        // if (!email || email.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g)) {
-            this.setState(() => ({ email }))
-        // }
+        const name = e.target.name;
+        const value = e.target.value;
+        console.log('name: ', name);
+        console.log('value: ', value);
+        this.setState(() => ({ [name]: value }))
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log('state: ', this.state)
+        let validemail = this.state.email;
+
+        console.log('on form submit: ')
+        console.log('email: ', validemail);
+        console.log('state email: ', this.state.email);
+
+        if (!this.state.email || !validemail.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g)) {
+            console.log('email not valid')
+            this.setState(() => ({ emailError: true }));
+        }
+        else {
+            console.log('valid email ==========');
+            this.setState(() => ({ emailError: false }));
+        }
+        // TODO: enable password validation to check min number of characters
+
+        // TODO: send form data to consumer
+
+        // TODO: cleanup, refactor, remove comments
     }
 
     render() {
@@ -21,14 +48,18 @@ class LoginForm extends Component {
                     <input
                         type='text'
                         placeholder="email"
+                        name="email"
+                        ref="name"
                         onChange={e => this.onChange(e)}
                     />
                     <input
-                        type='password'
+                        type='text'
                         placeholder="password"
+                        name="pwd"
+                        ref="name"
                         onChange={e => this.onChange(e)}
                     />
-                    <button>
+                    <button onClick={e => this.onFormSubmit(e)}>
                         Log In
                     </button>
                 </form>
