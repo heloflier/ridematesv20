397c625f5e8aba1d92d8c26568ce30dd1d6fdadb
Fabio <heloflyer@hotmail.com>
2019-05-16 12:32:09 -0700

    added first draft of profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserProfile from './components/UserProfile';
 
 function App() {
     return (
         <div className="App">
             Ridemates v 2.0
+            <UserProfile />
     </div>
     );
 }
