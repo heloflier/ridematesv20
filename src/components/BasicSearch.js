a71a50f3055f09abf645ac18492552f9548ec4d2
Fabio <heloflyer@hotmail.com>
2019-05-24 11:50:40 -0700

    edited initial basic search form

--- src/components/BasicSearch.js
+++ src/components/BasicSearch.js
@@ -9,20 +9,12 @@ class BasicSearch extends Component {
     };
 
     onSubmit = e => {
-        console.log('basic search on form submit')
         e.preventDefault();
-        // let type =
-
-        //     this.setState({ [name]: value });
-
-        console.log('state: ', this.state)
     }
 
     onChange = e => {
         let value = e.target.value
         let name = e.target.name
-        console.log('on change name: ', name)
-        console.log('on change value: ', value)
         this.setState({ [name]: value });
     };
 
@@ -74,73 +66,3 @@ class BasicSearch extends Component {
 }
 
 export default BasicSearch;
-
-// required
-// id="where-id"
-// label="Where"
-// className={classes.textField}
-// value={this.state.where}
-// onChange={this.handleChange("where")}
-// placeholder="Destination"
-// margin="none"
-// fullWidth
-// />
-// <br />
-// <br />
-// <FormControl className={classes.formControl} margin="none">
-// <InputLabel htmlFor="bike-type-simple">Type of Biking</InputLabel>
-// <Select
-//     id="bike-type"
-//     fullWidth
-//     value={this.state.biketype}
-//     onChange={this.handleChange("biketype")}
-//     input={<Input id="bike-type-simple" />}
-// >
-//     <MenuItem value="">
-//         <em>None</em>
-//     </MenuItem>
-//     <MenuItem value="Mountain Bike">Mountain Bike</MenuItem>
-//     <MenuItem value="Road Bike">Road Bike</MenuItem>
-//     <MenuItem value="Hybrid">Hybrid</MenuItem>
-// </Select>
-// </FormControl>
-// <br />
-// <br />
-// <FormControl className={classes.formControl} margin="none">
-// <InputLabel htmlFor="age-simple">Level of Difficulty</InputLabel>
-// <Select
-//     id="difficulty"
-//     fullWidth
-//     value={this.state.difficulty}
-//     onChange={this.handleChange("difficulty")}
-//     input={<Input id="difficulty-simple" />}
-// >
-//     <MenuItem value="">
-//         <em>None</em>
-//     </MenuItem>
-//     <MenuItem value="Easy (15 - 25 miles)">
-//         Easy (15 - 25 miles)
-// </MenuItem>
-//     <MenuItem value="Intermediate (25 - 45 miles)">
-//         Intermediate (25 - 45 miles)
-// </MenuItem>
-//     <MenuItem value="Hard (Above 50 miles)">
-//         Hard (Above 50 miles)
-// </MenuItem>
-// </Select>
-// </FormControl>
-// </CardContent>
-
-// <CardActions>
-
-// <Button
-// color="primary"
-// className="rm-main-button"
-// onClick={this.submitChange}
-// >
-// Find Mates!
-// </Button>
-
-
-// </CardActions>
-// </Card >
